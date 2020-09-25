b77fe78ac26944edbff3a1906c12bb808050e98c
Sampsa Riikonen <sampsa.riikonen@iki.fi>
2020-09-25 21:10:29 +0300

    etc

--- ext/configure_live.bash
+++ ext/configure_live.bash
@@ -3,18 +3,20 @@ cp config.linux-generic live/
 cd live
 ## this doesn't work at PPA's autobuild:
 #arch=$(uname --m)
-#if [[ $arch = "x86_64" ]];
-#then
-#    echo "LIVE555: using linux-64bit"
-#    ./genMakefiles linux-64bit
-#elif [[ $arch = *"arm"* ]];
-#then
-#    echo "LIVE555: using armlinux"
-#    ./genMakefiles armlinux
-#else
-#    echo "LIVE555: WARNING: using generic linux"
-#    ./genMakefiles linux-generic
-#fi
+arch=$1
+echo "CONFIGURE_LIVE: ARCH "$arch
+if [[ $arch = "x86_64" || $arch = "amd64" ]];
+then
+    echo "LIVE555: using linux-64bit"
+    ./genMakefiles linux-64bit
+elif [[ $arch = *"arm"* ]];
+then
+    echo "LIVE555: using armlinux"
+    ./genMakefiles armlinux
+else
+    echo "LIVE555: WARNING: using generic linux"
+    ./genMakefiles linux-generic
+fi
 ## so we'll skip it & don't build for arm for the moment..
-./genMakefiles linux-64bit
+#./genMakefiles linux-64bit
 cd ..
